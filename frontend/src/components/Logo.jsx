21a0797d6ba0aa095bff37f3feb05ce6b45c057d
yelxam <yelxam@gmail.com>
2026-01-15 16:59:20 -0300

    style: replace SVG logo with new official image logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
+import logoImg from '../assets/logo.png';
 
 const Logo = ({ className = "h-10", showText = true }) => {
     return (
         <div className={`flex items-center gap-3 ${className}`}>
-            <div className="relative h-full aspect-square flex items-center justify-center">
-                <svg viewBox="0 0 100 100" className="h-full w-full" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    {/* Microphone Icon - Based on the new logo */}
-                    <g className="text-indigo-600 dark:text-white" fill="currentColor">
-                        {/* Mic Head */}
-                        <rect x="38" y="25" width="24" height="35" rx="12" />
-
-                        {/* Cradle */}
-                        <path d="M33 50V52C33 61.3888 40.6112 69 50 69C59.3888 69 67 61.3888 67 52V50H63V52C63 59.1797 57.1797 65 50 65C42.8203 65 37 59.1797 37 52V50H33Z" />
-
-                        {/* Stand */}
-                        <rect x="48" y="69" width="4" height="10" />
-                        <rect x="40" y="79" width="20" height="4" rx="2" />
-
-                        {/* Arrow Sweeping Around */}
-                        <path
-                            d="M25 65C15 50 20 25 50 20C75 15 85 40 80 60"
-                            stroke="currentColor"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                            fill="none"
-                        />
-                        {/* Arrow Head */}
-                        <path d="M75 12L85 20L72 30Z" />
-                    </g>
-                </svg>
+            <div className="relative h-full flex items-center justify-center">
+                <img
+                    src={logoImg}
+                    alt="VerboCast Logo"
+                    className="h-full w-auto object-contain dark:brightness-100 brightness-0 invert-[.1]"
+                />
             </div>
 
             {showText && (
